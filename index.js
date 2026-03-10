fcf2e7d2a3324d10a2fda3aedf38c72f8b7dcb8d
Julia Farkun <julia.farkun@yandex.ru>
2026-03-10 18:19:23 +0500

    Tasks 3-4

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ function animaster() {
             element.classList.add('show');
         },
 
+        fadeOut(element, duration) {
+            element.style.transitionDuration = `${duration}ms`;
+            element.classList.remove('show');
+            element.classList.add('hide');
+        },
+
+
         /**
          * Функция, передвигающая элемент
          * @param {HTMLElement} element — HTMLElement, который надо анимировать
@@ -37,6 +44,32 @@ function animaster() {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
         },
+
+        moveAndHide(element, duration, translation) {
+            this.move(element, (duration * 2) / 5, translation);
+            setTimeout(() => {
+                this.fadeOut(element, (duration * 3) / 5);
+            }, (duration * 2) / 5);
+        },
+
+        showAndHide(element, duration) {
+            const part = duration / 3;
+            this.fadeIn(element, part);
+            setTimeout(() => {
+                this.fadeOut(element, part);
+            }, part * 2);
+        },
+
+        heartBeating(element) {
+            const beat = () => {
+                this.scale(element, 500, 1.4);
+                setTimeout(() => {
+                    this.scale(element, 500, 1);
+                }, 500);
+            };
+            beat();
+            return setInterval(beat, 1000);
+        }
     };
 }
 
@@ -67,6 +100,12 @@ function addListeners() {
             master.fadeIn(block, 5000);
         });
 
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            master.fadeOut(block, 5000);
+        });
+
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
@@ -77,5 +116,23 @@ function addListeners() {
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
             master.scale(block, 1000, 1.25);
+        });
+
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            master.moveAndHide(block, 5000, {x: 100, y: 10});
+        });
+
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            master.showAndHide(block, 5000, {x: 100, y: 10});
+        });
+    
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            master.heartBeating(block, 5000, {x: 100, y: 10});
         });
 }
